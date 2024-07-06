d9f7a4e3669d58d44a54c8a93be36149bca6373a
Daysi Cruz <daysicruz960@gmail.com>
2024-07-06 10:29:59 -0600

    Express Server & Initial Product

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import products from './data/products';
+
+const port =  5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running...')
+});
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+})
+app.get('/api/products:id', (req, res) => {
+    const products = products.find((p) => p._id === req.params.id)
+    res.json(products);
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
